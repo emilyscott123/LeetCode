61decbc62c12da28bfa436be3360a82b2cc68221
Emily Scott <64emily@gmail.com>
2023-06-15 00:47:25 -0400

    Time: 1001 ms (29.69%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+select weather.id as "Id"
+from
+    weather 
+        join 
+    weather w on datediff(weather.recordDate, w.recordDate) = 1
+        and weather.Temperature > w.Temperature
+;
